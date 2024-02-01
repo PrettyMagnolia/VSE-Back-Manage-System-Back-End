bef73c60b66c1e75edc2eceee27d13e0d6b2c1c2
Chennuo <chennuo812@qq.com>
2024-02-01 20:13:14 +0800

    3

--- src/main/java/com/backend/vse/controller/ReportController.java
+++ src/main/java/com/backend/vse/controller/ReportController.java
@@ -126,10 +126,14 @@ public class ReportController {
                     result.insert(result.length() - 1, '\\');
                 }
             }
-//            System.out.println(result);
+            System.out.println(result);
 //            System.out.println("python "+pythonScriptPath +" "+"\""+result+"\"");
-
-            proc = Runtime.getRuntime().exec("python "+pythonScriptPath +" "+"\""+result+"\"");
+//            if("production".equals(System.getProperty("env"))){
+//                String[] cmd = { "python", pythonScriptPath, jsonString };
+//                proc = Runtime.getRuntime().exec(cmd);
+//            }else{
+                proc = Runtime.getRuntime().exec("python "+pythonScriptPath +" "+"\""+result+"\"");
+//            }
             BufferedReader in = new BufferedReader(new InputStreamReader(proc.getInputStream()));
             String line = null;
             String returnCode = "";
