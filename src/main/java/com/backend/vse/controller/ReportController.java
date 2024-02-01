25dc7609c73c301c615810bf05e05446c417c85a
Chennuo <chennuo812@qq.com>
2024-02-01 19:30:14 +0800

    1

--- src/main/java/com/backend/vse/controller/ReportController.java
+++ src/main/java/com/backend/vse/controller/ReportController.java
@@ -105,9 +105,10 @@ public class ReportController {
         String pythonScriptPath = currentWorkingDirectory+"/src/main/resources/"+"fillExpTemplate.py";
         // 若当前环境为生产环境
         if("production".equals(System.getProperty("env"))){
-            pythonScriptPath = "/workspace/fillExpTemplate.py";
+            pythonScriptPath =currentWorkingDirectory+ "fillExpTemplate.py";
         }
         System.out.println(pythonScriptPath);
+        System.out.println(currentWorkingDirectory);
         try {
 //            ObjectMapper objectMapper = new ObjectMapper();
 //            String jsonString = objectMapper.writeValueAsString(dynamicJson);
